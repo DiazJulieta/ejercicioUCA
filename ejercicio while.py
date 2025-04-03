625605f48e6b687dcb118048233d18650026728e
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-03 19:28:25 -0300

    Add files via upload

---
+++ ejercicio while.py
@@ -0,0 +1,17 @@
+#while repeti un bloque mientras una condicion sea verdadera
+
+contador=0
+
+while contador <5:
+    print(contador)
+    contador +=1
+#contador = 3 // asigno un valor a la variable
+#contador == 5// hacer una comparacion
+   
+contador=0 
+while True:
+    print(contador)
+    contador +=1
+    if contador == 5:
+        break
+    
