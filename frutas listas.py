580b92defb99e9d849eb45567d1e3d722f4aa901
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-03 19:24:14 -0300

    Add files via upload

---
+++ frutas listas.py
@@ -0,0 +1,12 @@
+frutas = ["manzana","pera","uva","mandarina"]
+#frutas.append("banana")
+#print(frutas[0])
+
+frutas.insert(2, "durazno")
+frutas.remove("uva")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:
+    print(fruta)
+  
+
