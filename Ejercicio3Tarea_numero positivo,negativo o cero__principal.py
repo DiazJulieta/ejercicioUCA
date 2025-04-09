8efbe627aa0fd28edf8788c1ab7abb7025acdd59
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-09 18:33:46 -0300

    Add files via upload

---
+++ Ejercicio3Tarea_numero positivo,negativo o cero__principal.py
@@ -0,0 +1,21 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+numero=int(input("Ingrese un numero: "))
+
+if numero>0:
+    print("El numero es positivo")
+elif numero==0:
+    print("El numero es cero")
+else:
+    print("El numero es negativo")
+
+otronumero=int(input("Ingrese un numero: "))
+
+if otronumero>0:
+    print("El numero es positivo")
+elif otronumero==0:
+    print("El numero es cero")
+else:
+    print("El numero es negativo")
+    
