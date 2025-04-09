edbbf082be87282e66178f9ebdcd85e6304ab6d3
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-09 19:21:32 -0300

    Add files via upload

---
+++ Ejercicio4Tarea_numero par o impar__principal.py
@@ -0,0 +1,11 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+numero = input("ingrese un numero")
+numero = int(numero)
+numero = (numero%2)
+
+if numero > 0 :
+    print("es numero impar")
+else:
+    print("es numero Par")
+    
