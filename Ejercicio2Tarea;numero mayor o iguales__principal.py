79afe9befb81143bf43a89c13637919847dc19eb
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-09 19:03:20 -0300

    Add files via upload

---
+++ Ejercicio2Tarea;numero mayor o iguales__principal.py
@@ -0,0 +1,15 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+a = input("ingrese el primer valor") 
+b = input("ingrese el segundo valor")
+
+if a>b:
+    print("el primer numero es mayor")
+elif b>a:
+    print("el segundo numero es mayor")
+    
+else:
+    print("Iguales")
+    
+
+    
