807850860e22b0c4758fe692049de2f6e67df435
DiazJulieta <julietadiaz1@uca.edu.ar>
2025-04-10 19:48:05 -0300

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,11 @@
+
+persona= {"nombre":"rafael","edad":49,"cuidad":"paraná"}
+print(persona["nombre"])
+    
+#lista de 6 valores aleatorios (0,45)
+#lista de numero que yo eliga 
+#simular con el random, elegir un numero y buscarlo en mi lista, 
+#si ese numero esta en la lista borrarlo y ponerlo en la otra
+
+for i in range(20):
+    numero = random.randint(1,100)
